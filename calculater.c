854e82c33a57e9c3f92c8b1ca427b0bae226d7ee
Jindahye <jindahye0315_@naver.com>
2025-04-04 16:01:35 +0900

    multiply 함수 추가 및 곱셈 기능 구현현

--- calculater.c
+++ calculater.c
@@ -1,9 +1,13 @@
+int multiply(int x, int y) {
+    return x * y;
+}
+
 int main()
 {
     printf("1:+, 2:-, 3:*, 4:/, 숫자를 선택해주세요 :\n");
     scanf("%d", 연산자 선택을 받아줄 변수);
 
-    printf("두 수를 입력하세요 :\n);
+    printf("두 수를 입력하세요 :\n");
     scanf("%d %d", 계산할 두 개의 숫자를 받아줄 변수);
     
     // 나머진 혼자 만들 수 있겠죠??
