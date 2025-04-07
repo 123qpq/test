05f9d6509d8d9f4fb3fd17e929e363c4e1e1e0b0
Jindahye <jindahye0315_@naver.com>
2025-04-07 22:20:19 +0900

    입력 예외 처리

--- calculater.c
+++ calculater.c
@@ -15,9 +15,18 @@ int main()
 {
     int x, y;
     int choice;
+    
+    while (1) {
+        printf("1:+, 2:-, 3:*, 4:/, 숫자를 선택해주세요 :\n");
+        scanf("%d", &choice);
 
-    printf("1:+, 2:-, 3:*, 4:/, 숫자를 선택해주세요 :\n");
-    scanf("%d", &choice);
+        if (choice >= 1 && choice <= 4) {
+            break;
+        }
+
+        printf("1~4의 숫자를 선택 해주세요.\n");
+    }
+    
 
     printf("두 수를 입력하세요 :\n");
     scanf("%d %d", &x, &y);
